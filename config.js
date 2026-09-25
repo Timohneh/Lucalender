fefa1fdd7be5317195e72ed8e8aa6f3209fb00e2
Gabriel Sauter <gabrielsauter4@gmail.com>
2026-09-25 13:05:02 +0200

    Connect Lucalender to Supabase

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 window.LUCALENDER_CONFIG = {
-  supabaseUrl: "",
-  supabaseAnonKey: "",
+  supabaseUrl: "https://usvixphzlelczxuzydak.supabase.co",
+  supabaseAnonKey: "sb_publishable_fbRWm3CENo1C2fPGIFg9xQ_8VBieN3h",
 };
